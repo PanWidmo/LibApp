8378308b2d93d97b1cbe70d531ac6b9860782694
PanWidmo <tomasz.baranww@gmail.com>
2021-12-12 11:27:07 +0100

    Modified CustomerFormViewModel

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -46,9 +46,8 @@ namespace LibApp.Controllers
         {
             var membershipTypes = _context.MembershipTypes.ToList();
 
-            var viewModel = new CustomerFormViewModel
+            var viewModel = new CustomerFormViewModel()
             {
-                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             return View("CustomerForm", viewModel);
@@ -62,9 +61,8 @@ namespace LibApp.Controllers
                 return NotFound();
             }
 
-            var viewModel = new CustomerFormViewModel
+            var viewModel = new CustomerFormViewModel(customer)
             {
-                Customer = customer,
                 MembershipTypes = _context.MembershipTypes.ToList()
             };
 
@@ -78,9 +76,8 @@ namespace LibApp.Controllers
         {
             if (!ModelState.IsValid)
             {
-                var viewModel = new CustomerFormViewModel
+                var viewModel = new CustomerFormViewModel(customer)
                 {
-                    Customer = customer,
                     MembershipTypes = _context.MembershipTypes.ToList()
                 };
 
