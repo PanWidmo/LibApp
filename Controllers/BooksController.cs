30807a77443aa07c22f5f898fae0324e1dde20df
PanWidmo <tomasz.baranww@gmail.com>
2022-01-25 15:01:18 +0100

    Added Edit to BookController. Also added preventDefault() to NewBook

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -12,14 +12,12 @@ namespace LibApp.Controllers
 {
     public class BooksController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public BooksController(ApplicationDbContext context)
+        public ViewResult Index()
         {
-            _context = context;
+            return View();
         }
 
-        public ViewResult Index()
+        public ViewResult New()
         {
             return View();
         }
@@ -29,69 +27,10 @@ namespace LibApp.Controllers
             return View();
         }
 
-        public IActionResult Edit(int id)
-        {
-            var book = _context.Books.SingleOrDefault(b => b.Id == id);
-
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            var viewModel = new BookFormViewModel (book)
-            {
-                Genres = _context.Genres.ToList()
-            };
-
-            return View("BookForm", viewModel);
-        }
-
-        public ActionResult New()
+        public ViewResult Edit()
         {
             return View();
-
-            /*var genres = _context.Genres.ToList();*/
-
-            /*var viewModel = new BookFormViewModel()
-            {
-                Genres = genres
-            };*/
-
-            /*return View("BookForm", viewModel);*/
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Save(Book book)
-        {
-            if (book.Id == 0)
-            {
-                _context.Books.Add(book);
-            }
-            else
-            {
-                var bookInDb = _context.Books.Single(c => c.Id == book.Id);
-                bookInDb.Name = book.Name;
-                bookInDb.AuthorName = book.AuthorName;
-                bookInDb.GenreId = book.GenreId;
-                bookInDb.NumberInStock = book.NumberInStock;
-            }
-
-
-            try
-            {
-                _context.SaveChanges();
-            }
-
-            catch (DbUpdateException e)
-            {
-                Console.WriteLine(e);
-            }
-
-
-            return RedirectToAction("Index", "Books");
-        }
-
 
     }
 }
