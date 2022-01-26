71ca4c46d4d13073f48645829959ddd0a3ca886b
PanWidmo <tomasz.baranww@gmail.com>
2022-01-26 14:49:42 +0100

    Added Edit to CustomerController. Changes in New/Edit forms

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -24,77 +24,20 @@ namespace LibApp.Controllers
             return View();
         }
 
-        public ViewResult Details()
-        {
-            return View();
-        }
-
         public ViewResult New()
         {
             return View();
         }
 
-        public IActionResult Edit(int id)
+        public ViewResult Details()
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
-            if(customer == null)
-            {
-                return NotFound();
-            }
-
-            var viewModel = new CustomerFormViewModel(customer)
-            {
-                MembershipTypes = _context.MembershipTypes.ToList(),
-                RoleTypes = _context.RoleTypes.ToList()
-            };
-
-            return View("CustomerForm", viewModel);
+            return View();
         }
-        
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Save(Customer customer)
+        public ViewResult Edit()
         {
-            if (!ModelState.IsValid)
-            {
-                var viewModel = new CustomerFormViewModel(customer)
-                {
-                    MembershipTypes = _context.MembershipTypes.ToList(),
-                    RoleTypes = _context.RoleTypes.ToList()
-                };
 
-                return View("CustomerForm", viewModel);
-            }
-
-            if(customer.Id == 0)
-            {
-                _context.Customers.Add(customer);
-            }
-            else
-            {
-                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
-                customerInDb.Name = customer.Name;
-                customerInDb.Email = customer.Email;
-                customerInDb.PasswordHash = customer.PasswordHash;
-                customerInDb.Birthdate = customer.Birthdate;
-                customerInDb.MembershipTypeId = customer.MembershipTypeId;
-                customerInDb.HasNewsletterSubscribed = customer.HasNewsletterSubscribed;
-                customerInDb.RoleTypeId = customer.RoleTypeId;
-            }
-
-            try
-            {
-                _context.SaveChanges();
-            }
-
-            catch (DbUpdateException e)
-            {
-                Console.WriteLine(e);
-            }
-
-
-            return RedirectToAction("Index", "Customers");
+            return View();
         }
 
 
