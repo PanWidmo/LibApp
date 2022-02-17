238da21fe0618743ad514e953a2912d68657e294
PanWidmo <tomasz.baranww@gmail.com>
2022-02-17 13:40:39 +0100

    Added Authorization to api BooksController

--- Controllers/Api/BooksController.cs
+++ Controllers/Api/BooksController.cs
@@ -23,6 +23,7 @@ namespace LibApp.Controllers.Api
 
         //GET /api/books
         [HttpGet]
+        [Authorize(Roles = "User, StoreManager, Owner")]
         public IActionResult GetAllBooks()
         {
             var result = bookService.GetAllBooks();
@@ -32,6 +33,7 @@ namespace LibApp.Controllers.Api
 
         //GET /api/books/{id}
         [HttpGet("{id}")]
+        [Authorize(Roles = "User, StoreManager, Owner")]
         public IActionResult GetBookById(int id)
         {
             var result = bookService.GetBookById(id);
@@ -41,6 +43,7 @@ namespace LibApp.Controllers.Api
 
         //POST /api/books
         [HttpPost]
+        [Authorize(Roles = "StoreManager, Owner")]
         public IActionResult CreateNewBook(BookUpdateCreateDto createBookDto)
         {
             var result = bookService.CreateNewBook(createBookDto);
@@ -50,6 +53,7 @@ namespace LibApp.Controllers.Api
 
         //PUT /api/books
         [HttpPut("{id}")]
+        [Authorize(Roles = "StoreManager, Owner")]
         public IActionResult UpdateBook(int id, BookUpdateCreateDto updateBookDto)
         {
             bookService.UpdateBook(id, updateBookDto);
@@ -59,6 +63,7 @@ namespace LibApp.Controllers.Api
 
         //DELETE /api/books
         [HttpDelete("{id}")]
+        [Authorize(Roles = "StoreManager, Owner")]
         public IActionResult DeleteBook(int id)
         {
             bookService.DeleteBook(id);
